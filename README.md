a628d3e89c4444256e4cae55db91a11aaa3b76dc
kttngo <katieeengooo@gmail.com>
2026-05-04 14:59:51 -0700

    finish expose section

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lab 5 - Starter
 Name: Katie Ngo
+## Live Demo
+[View Expose Page](https://kttngo.github.io/Lab5_Starter/expose.html)
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,41 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const confetti = new JSConfetti();
+
+  const audio = document.querySelector("audio");
+  const button = document.querySelector("button");
+  const volIcon = document.querySelector("#volume-controls img");
+  const volSlider = document.querySelector("#volume");
+  const hornImg = document.querySelector("#expose img");
+  const hornSelect = document.querySelector("#horn-select");
+
+  // Horn
+  hornSelect.addEventListener("change", function () {
+    hornImg.src = `assets/images/${hornSelect.value}.svg`;
+    audio.src = `assets/audio/${hornSelect.value}.mp3`;
+  });
+
+  // Volume
+  volSlider.addEventListener("input", function () {
+    const vol = volSlider.value;
+    audio.volume = vol / 100;
+    if (vol == 0) {
+      volIcon.src = "assets/icons/volume-level-0.svg";
+    } else if (vol < 33) {
+      volIcon.src = "assets/icons/volume-level-1.svg";
+    } else if (vol < 67) {
+      volIcon.src = "assets/icons/volume-level-2.svg";
+    } else {
+      volIcon.src = "assets/icons/volume-level-3.svg";
+    }
+  });
+
+  // Sound & Confetti
+  button.addEventListener("click", function () {
+    audio.play();
+    if (hornSelect.value == "party-horn") {
+      confetti.addConfetti();
+    }
+  });
 }
